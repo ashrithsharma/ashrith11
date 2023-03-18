1ae566c6404fa92ad58c87431afa7847ee2ffd83
ashrithsharma <119427194+ashrithsharma@users.noreply.github.com>
2023-03-18 08:23:45 +0530

    Add files via upload

---
+++ xygreat.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+class ashrith
+{
+public:
+int x,y;
+void great()
+{
+cin>>x>>y;
+iF(x>y)
+{
+cout<<"x is greater";
+}
+else
+{
+cout<<"Y is greater";
+}
+};
+
+void main()
+{
+ashrith a;
+a.great();
+}
